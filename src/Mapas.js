38a2c72db8439374bedf394be22237de4b94c9ae
Alberth-h <jesalbertohyfx@hotmail.com>
2020-10-25 16:45:38 -0700

    mapitas

--- src/Mapas.js
+++ src/Mapas.js
@@ -1,152 +1,301 @@
 import React from 'react'
+import Map from './components/map';
 
-export const Mapas = () => (
-    <div>
-        <h2>Mapas </h2>
-        <p>2.1) Red inspeccionada</p>
-        <p>Se inspeccionaron las redes COT04010 y LST OVI 73510 COT Administradas por CFE que se encuentran en el área cercana a la presa el Oviachic.</p>
-        <p>En este mapa se pueden ver ambas polilíneas de redes, también los puntos de las estructuras. Dar click a los círculos en los mapas para ver detalles</p>
-        <p>2.2) Imágenes tomadas con Drones</p>
-        <p>Aquí se muestran las ubicaciones geográficas extraídas de los metadatos de las imágenes de dónde fueron capturadas con ambas cámaras con el drone, en Violeta las imágenes tomadas con la Cámara de color real (RGB) y en Rojo las tomadas con la cámara Termica (FLIR). Dar click a los círculos en los mapas para ver detalles</p>
-        <p>2.3) Mapa de Imágenes con Anomalías</p>
-        <p>Mapa de estructuras de electricidad en los cuales se detectaron anomalías usando inteligencia artificial, dar click a los círculos en los mapas para ver detalles.</p>
-        <p>En esta tabla se muestra la liga a seguir de la imagen color real, La imagen segmentada, La imagen detectada por el modelo de inteligencia artificial, la imagen térmica y los reportes individuales.</p>
-        <table>
-            <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>ImagenRGB</th>
-            <th>Segmentacion</th>
-            <th>Deteccion</th>
-            <th>Termica</th>
-            <th>Reporte</th>
-            </tr>
-            <tr>
-            <td>947</td>
-            <td>COT04010</td>
-            <td><a href="https://cutt.ly/goJPrBJ" target="_blank">https://cutt.ly/goJPrBJ</a></td>     
-            <td><a href="https://cutt.ly/CfhV6Pc" target="_blank">https://cutt.ly/CfhV6Pc</a></td>
-           <td><a href="https://cutt.ly/WoNqin2" target="_blank">https://cutt.ly/WoNqin2</a></td>
-            <td>NA</td>
-           <td><a href="https://cutt.ly/8ptmC0B" target="_blank">https://cutt.ly/8ptmC0B</a></td>
-            </tr>
-           <tr>
-           <td>934</td>
-           <td>COT04010</td>
-           <td><a href="https://cutt.ly/uoJPqwl" target="_blank">https://cutt.ly/uoJPqwl</a></td>
-           <td><a href="https://cutt.ly/wfhBfrf" target="_blank">https://cutt.ly/wfhBfrf</a></td>
-           <td><a href="https://cutt.ly/hoB66BP" target="_blank">https://cutt.ly/hoB66BP</a></td>
-            <td>NA</td>
-           <td><a href="https://cutt.ly/pptlt5s" target="_blank">https://cutt.ly/pptlt5s</a></td>
-            </tr>
-           <tr>
-           <td>922</td>
-           <td>COT04010</td>
-           <td><a href="https://cutt.ly/QoJO8vc" target="_blank">https://cutt.ly/QoJO8vc</a></td>
-           <td><a href="https://cutt.ly/VfhBamD" target="_blank">https://cutt.ly/VfhBamD</a></td>
-           <td><a href="https://cutt.ly/moB61DJ" target="_blank">https://cutt.ly/moB61DJ</a></td>
-            <td>NA</td>
-           <td><a href="https://cutt.ly/UptgiPi" target="_blank">https://cutt.ly/UptgiPi</a></td>
-            </tr>
-           <tr>
-           <td>874</td>
-           <td>COT04010</td>
-           <td><a href="https://cutt.ly/1oJOMN7" target="_blank">https://cutt.ly/1oJOMN7</a></td>
-           <td><a href="https://cutt.ly/lfhBenH" target="_blank">https://cutt.ly/lfhBenH</a></td>
-           <td><a href="https://cutt.ly/fptqlwq" target="_blank">https://cutt.ly/fptqlwq</a></td>
-            <td>NA</td>
-           <td><a href="https://cutt.ly/aptwrUZ" target="_blank">https://cutt.ly/aptwrUZ</a></td>
-            </tr>
-           <tr>
-            <td>718</td>
-            <td>COT04010</td>
-            <td><a href="https://cutt.ly/3oJOL5N" target="_blank">https://cutt.ly/3oJOL5N</a></td>
-            <td><a href="https://cutt.ly/TfhBikw" target="_blank">https://cutt.ly/TfhBikw</a></td>
-            <td><a href="https://cutt.ly/8oB6PsC" target="_blank">https://cutt.ly/8oB6PsC</a></td>
-            <td>NA</td>
-            <td><a href="https://cutt.ly/Cpr5jFy" target="_blank">https://cutt.ly/Cpr5jFy</a></td>
-            </tr>
-            <tr>
-            <td>3066</td>
-            <td>COT04010</td>
-            <td><a href="https://cutt.ly/goJOF6q" target="_blank">https://cutt.ly/goJOF6q</a></td>
-            <td><a href="https://cutt.ly/SfhBqYM" target="_blank">https://cutt.ly/SfhBqYM</a></td>
-            <td><a href="https://cutt.ly/ioB6bhY" target="_blank">https://cutt.ly/ioB6bhY</a></td>
-            <td>NA</td>
-            <td><a href="https://cutt.ly/hpr5idK" target="_blank">https://cutt.ly/hpr5idK</a></td>
-            </tr>
-            <tr>
-            <td>3054</td>
-            <td>COT04010</td>
-            <td><a href="https://cutt.ly/NoJOOtZ" target="_blank">https://cutt.ly/NoJOOtZ</a></td>
-            <td><a href="https://cutt.ly/ufhBgjm" target="_blank">https://cutt.ly/ufhBgjm</a></td>
-            <td><a href="https://cutt.ly/noB6jyS" target="_blank">https://cutt.ly/noB6jyS</a></td>
-            <td>NA</td>
-            <td><a href="https://cutt.ly/xpr73ND" target="_blank">https://cutt.ly/xpr73ND</a></td>
-            </tr>
-            <tr>
-            <td>2989</td>
-            <td>COT04010</td>
-            <td><a href="https://cutt.ly/8o6a0KG" target="_blank">https://cutt.ly/8o6a0KG</a></td>
-            <td><a href="https://cutt.ly/tfhBdhz" target="_blank">https://cutt.ly/tfhBdhz</a></td>
-            <td><a href="https://cutt.ly/Co6pbP3" target="_blank">https://cutt.ly/Co6pbP3</a></td>
-            <td><a href="https://cutt.ly/so6sfJ7" target="_blank">https://cutt.ly/so6sfJ7</a></td>
-            <td><a href="https://cutt.ly/nptmG1x" target="_blank">https://cutt.ly/nptmG1x</a></td>
-            </tr>
-            <tr>
-            <td>2730</td>
-            <td>COT04010</td>
-            <td><a href="https://cutt.ly/4oJPoMC" target="_blank">https://cutt.ly/4oJPoMC</a></td>
-            <td><a href="https://cutt.ly/vfhBiMW" target="_blank">https://cutt.ly/vfhBiMW</a></td>
-            <td><a href="https://cutt.ly/roNqgIO" target="_blank">https://cutt.ly/roNqgIO</a></td>
-            <td><a href="https://cutt.ly/2peMNAv" target="_blank">https://cutt.ly/2peMNAv</a></td>
-            <td><a href="https://cutt.ly/Epe7WC9" target="_blank">https://cutt.ly/Epe7WC9</a></td>
-            </tr>
-            <tr>
-            <td>2712</td>
-            <td>COT04010</td>
-            <td><a href="https://cutt.ly/Wpe2P5L" target="_blank">https://cutt.ly/Wpe2P5L</a></td>
-            <td><a href="https://cutt.ly/hfhVOlp" target="_blank">https://cutt.ly/hfhVOlp</a></td>
-            <td><a href="https://cutt.ly/ype7qVa" target="_blank">https://cutt.ly/ype7qVa</a></td>
-            <td><a href="https://cutt.ly/Tpe4gBc" target="_blank">https://cutt.ly/Tpe4gBc</a></td>
-            <td><a href="https://cutt.ly/Ipr7LNa" target="_blank">https://cutt.ly/Ipr7LNa</a></td>
-            </tr>
-            <tr>
-            <td>2674</td>
-            <td>COT04010</td>
-            <td><a href="https://cutt.ly/3oJOoIu" target="_blank">https://cutt.ly/3oJOoIu</a></td>
-            <td><a href="https://cutt.ly/VfhV7LH" target="_blank">https://cutt.ly/VfhV7LH</a></td>
-            <td><a href="https://cutt.ly/DoB5Wwp" target="_blank">https://cutt.ly/DoB5Wwp</a></td>
-            <td>NA</td>
-            <td><a href="https://cutt.ly/Dpr7vc2" target="_blank">https://cutt.ly/Dpr7vc2</a></td>
-            </tr>
-            <tr>
-            <td>2454</td>
-            <td>COT04010</td>
-            <td><a href="https://cutt.ly/oo6fVpK" target="_blank">https://cutt.ly/oo6fVpK</a></td>
-            <td><a href="https://cutt.ly/jfhV5cv" target="_blank">https://cutt.ly/jfhV5cv</a></td>
-            <td><a href="https://cutt.ly/to6dmAj" target="_blank">https://cutt.ly/to6dmAj</a></td>
-            <td><a href="https://cutt.ly/zo6gZAk" target="_blank">https://cutt.ly/zo6gZAk</a></td>
-            <td><a href="https://cutt.ly/nptmG1x" target="_blank">https://cutt.ly/nptmG1x</a></td>
-            </tr>
-            <tr>
-            <td>4</td>
-            <td>OVI73510</td>
-            <td><a href="https://cutt.ly/QpeZxAh" target="_blank">https://cutt.ly/QpeZxAh</a></td>
-            <td><a href="https://cutt.ly/GfhBwvz" target="_blank">https://cutt.ly/GfhBwvz</a></td>
-            <td><a href="https://cutt.ly/0peZRdU" target="_blank">https://cutt.ly/0peZRdU</a></td>
-            <td><a href="https://cutt.ly/9peXSME" target="_blank">https://cutt.ly/9peXSME</a></td>
-            <td><a href="https://cutt.ly/9ptiLq6" target="_blank">https://cutt.ly/9ptiLq6</a></td>
-            </tr>
-            <tr>
-            <td>1</td>
-            <td>OVI73510</td>
-            <td><a href="https://cutt.ly/FpeLSmN" target="_blank">https://cutt.ly/FpeLSmN</a></td>
-            <td><a href="https://cutt.ly/AfhBo7V" target="_blank">https://cutt.ly/AfhBo7V</a></td>
-            <td><a href="https://cutt.ly/4peZtv9" target="_blank">https://cutt.ly/4peZtv9</a></td>
-            <td><a href="https://cutt.ly/hpeXpxo" target="_blank">https://cutt.ly/hpeXpxo</a></td>
-            <td><a href="https://cutt.ly/JptfENs" target="_blank">https://cutt.ly/JptfENs</a></td>
-            </tr>
-        </table>
-    </div>
+export const Mapas = () => ( <
+        div >
+        <
+        h2 > Mapas < /h2> <
+        p > 2.1) Red inspeccionada < /p> <
+    p > Se inspeccionaron las redes COT04010 y LST OVI 73510 COT Administradas por CFE que se encuentran en el área cercana a la presa el Oviachic. < /p> <
+    p > En este mapa se pueden ver ambas polilíneas de redes,
+    también los puntos de las estructuras.Dar click a los círculos en los mapas para ver detalles < /p> <
+    Map / >
+    <
+    p > 2.2) Imágenes tomadas con Drones < /p> <
+    p > Aquí se muestran las ubicaciones geográficas extraídas de los metadatos de las imágenes de dónde fueron capturadas con ambas cámaras con el drone, en Violeta las imágenes tomadas con la Cámara de color real(RGB) y en Rojo las tomadas con la cámara Termica(FLIR).Dar click a los círculos en los mapas para ver detalles < /p> <
+    Map / >
+    <
+    p > 2.3) Mapa de Imágenes con Anomalías < /p> <
+    p > Mapa de estructuras de electricidad en los cuales se detectaron anomalías usando inteligencia artificial, dar click a los círculos en los mapas para ver detalles. < /p> <
+    Map / >
+    <
+    p > En esta tabla se muestra la liga a seguir de la imagen color real, La imagen segmentada, La imagen detectada por el modelo de inteligencia artificial, la imagen térmica y los reportes individuales. < /p> <
+    table >
+    <
+    tr >
+    <
+    th > Name < /th> <
+    th > Description < /th> <
+    th > ImagenRGB < /th> <
+    th > Segmentacion < /th> <
+    th > Deteccion < /th> <
+    th > Termica < /th> <
+    th > Reporte < /th> <
+    /tr> <
+    tr >
+    <
+    td > 947 < /td> <
+    td > COT04010 < /td> <
+    td > < a href = "https://cutt.ly/goJPrBJ"
+target = "_blank" > https: //cutt.ly/goJPrBJ</a></td>     
+    <
+    td > < a href = "https://cutt.ly/CfhV6Pc"
+target = "_blank" > https: //cutt.ly/CfhV6Pc</a></td>
+    <
+    td > < a href = "https://cutt.ly/WoNqin2"
+target = "_blank" > https: //cutt.ly/WoNqin2</a></td>
+    <
+    td > NA < /td> <
+    td > < a href = "https://cutt.ly/8ptmC0B"
+target = "_blank" > https: //cutt.ly/8ptmC0B</a></td>
+    <
+    /tr> <
+    tr >
+    <
+    td > 934 < /td> <
+    td > COT04010 < /td> <
+    td > < a href = "https://cutt.ly/uoJPqwl"
+target = "_blank" > https: //cutt.ly/uoJPqwl</a></td>
+    <
+    td > < a href = "https://cutt.ly/wfhBfrf"
+target = "_blank" > https: //cutt.ly/wfhBfrf</a></td>
+    <
+    td > < a href = "https://cutt.ly/hoB66BP"
+target = "_blank" > https: //cutt.ly/hoB66BP</a></td>
+    <
+    td > NA < /td> <
+    td > < a href = "https://cutt.ly/pptlt5s"
+target = "_blank" > https: //cutt.ly/pptlt5s</a></td>
+    <
+    /tr> <
+    tr >
+    <
+    td > 922 < /td> <
+    td > COT04010 < /td> <
+    td > < a href = "https://cutt.ly/QoJO8vc"
+target = "_blank" > https: //cutt.ly/QoJO8vc</a></td>
+    <
+    td > < a href = "https://cutt.ly/VfhBamD"
+target = "_blank" > https: //cutt.ly/VfhBamD</a></td>
+    <
+    td > < a href = "https://cutt.ly/moB61DJ"
+target = "_blank" > https: //cutt.ly/moB61DJ</a></td>
+    <
+    td > NA < /td> <
+    td > < a href = "https://cutt.ly/UptgiPi"
+target = "_blank" > https: //cutt.ly/UptgiPi</a></td>
+    <
+    /tr> <
+    tr >
+    <
+    td > 874 < /td> <
+    td > COT04010 < /td> <
+    td > < a href = "https://cutt.ly/1oJOMN7"
+target = "_blank" > https: //cutt.ly/1oJOMN7</a></td>
+    <
+    td > < a href = "https://cutt.ly/lfhBenH"
+target = "_blank" > https: //cutt.ly/lfhBenH</a></td>
+    <
+    td > < a href = "https://cutt.ly/fptqlwq"
+target = "_blank" > https: //cutt.ly/fptqlwq</a></td>
+    <
+    td > NA < /td> <
+    td > < a href = "https://cutt.ly/aptwrUZ"
+target = "_blank" > https: //cutt.ly/aptwrUZ</a></td>
+    <
+    /tr> <
+    tr >
+    <
+    td > 718 < /td> <
+    td > COT04010 < /td> <
+    td > < a href = "https://cutt.ly/3oJOL5N"
+target = "_blank" > https: //cutt.ly/3oJOL5N</a></td>
+    <
+    td > < a href = "https://cutt.ly/TfhBikw"
+target = "_blank" > https: //cutt.ly/TfhBikw</a></td>
+    <
+    td > < a href = "https://cutt.ly/8oB6PsC"
+target = "_blank" > https: //cutt.ly/8oB6PsC</a></td>
+    <
+    td > NA < /td> <
+    td > < a href = "https://cutt.ly/Cpr5jFy"
+target = "_blank" > https: //cutt.ly/Cpr5jFy</a></td>
+    <
+    /tr> <
+    tr >
+    <
+    td > 3066 < /td> <
+    td > COT04010 < /td> <
+    td > < a href = "https://cutt.ly/goJOF6q"
+target = "_blank" > https: //cutt.ly/goJOF6q</a></td>
+    <
+    td > < a href = "https://cutt.ly/SfhBqYM"
+target = "_blank" > https: //cutt.ly/SfhBqYM</a></td>
+    <
+    td > < a href = "https://cutt.ly/ioB6bhY"
+target = "_blank" > https: //cutt.ly/ioB6bhY</a></td>
+    <
+    td > NA < /td> <
+    td > < a href = "https://cutt.ly/hpr5idK"
+target = "_blank" > https: //cutt.ly/hpr5idK</a></td>
+    <
+    /tr> <
+    tr >
+    <
+    td > 3054 < /td> <
+    td > COT04010 < /td> <
+    td > < a href = "https://cutt.ly/NoJOOtZ"
+target = "_blank" > https: //cutt.ly/NoJOOtZ</a></td>
+    <
+    td > < a href = "https://cutt.ly/ufhBgjm"
+target = "_blank" > https: //cutt.ly/ufhBgjm</a></td>
+    <
+    td > < a href = "https://cutt.ly/noB6jyS"
+target = "_blank" > https: //cutt.ly/noB6jyS</a></td>
+    <
+    td > NA < /td> <
+    td > < a href = "https://cutt.ly/xpr73ND"
+target = "_blank" > https: //cutt.ly/xpr73ND</a></td>
+    <
+    /tr> <
+    tr >
+    <
+    td > 2989 < /td> <
+    td > COT04010 < /td> <
+    td > < a href = "https://cutt.ly/8o6a0KG"
+target = "_blank" > https: //cutt.ly/8o6a0KG</a></td>
+    <
+    td > < a href = "https://cutt.ly/tfhBdhz"
+target = "_blank" > https: //cutt.ly/tfhBdhz</a></td>
+    <
+    td > < a href = "https://cutt.ly/Co6pbP3"
+target = "_blank" > https: //cutt.ly/Co6pbP3</a></td>
+    <
+    td > < a href = "https://cutt.ly/so6sfJ7"
+target = "_blank" > https: //cutt.ly/so6sfJ7</a></td>
+    <
+    td > < a href = "https://cutt.ly/nptmG1x"
+target = "_blank" > https: //cutt.ly/nptmG1x</a></td>
+    <
+    /tr> <
+    tr >
+    <
+    td > 2730 < /td> <
+    td > COT04010 < /td> <
+    td > < a href = "https://cutt.ly/4oJPoMC"
+target = "_blank" > https: //cutt.ly/4oJPoMC</a></td>
+    <
+    td > < a href = "https://cutt.ly/vfhBiMW"
+target = "_blank" > https: //cutt.ly/vfhBiMW</a></td>
+    <
+    td > < a href = "https://cutt.ly/roNqgIO"
+target = "_blank" > https: //cutt.ly/roNqgIO</a></td>
+    <
+    td > < a href = "https://cutt.ly/2peMNAv"
+target = "_blank" > https: //cutt.ly/2peMNAv</a></td>
+    <
+    td > < a href = "https://cutt.ly/Epe7WC9"
+target = "_blank" > https: //cutt.ly/Epe7WC9</a></td>
+    <
+    /tr> <
+    tr >
+    <
+    td > 2712 < /td> <
+    td > COT04010 < /td> <
+    td > < a href = "https://cutt.ly/Wpe2P5L"
+target = "_blank" > https: //cutt.ly/Wpe2P5L</a></td>
+    <
+    td > < a href = "https://cutt.ly/hfhVOlp"
+target = "_blank" > https: //cutt.ly/hfhVOlp</a></td>
+    <
+    td > < a href = "https://cutt.ly/ype7qVa"
+target = "_blank" > https: //cutt.ly/ype7qVa</a></td>
+    <
+    td > < a href = "https://cutt.ly/Tpe4gBc"
+target = "_blank" > https: //cutt.ly/Tpe4gBc</a></td>
+    <
+    td > < a href = "https://cutt.ly/Ipr7LNa"
+target = "_blank" > https: //cutt.ly/Ipr7LNa</a></td>
+    <
+    /tr> <
+    tr >
+    <
+    td > 2674 < /td> <
+    td > COT04010 < /td> <
+    td > < a href = "https://cutt.ly/3oJOoIu"
+target = "_blank" > https: //cutt.ly/3oJOoIu</a></td>
+    <
+    td > < a href = "https://cutt.ly/VfhV7LH"
+target = "_blank" > https: //cutt.ly/VfhV7LH</a></td>
+    <
+    td > < a href = "https://cutt.ly/DoB5Wwp"
+target = "_blank" > https: //cutt.ly/DoB5Wwp</a></td>
+    <
+    td > NA < /td> <
+    td > < a href = "https://cutt.ly/Dpr7vc2"
+target = "_blank" > https: //cutt.ly/Dpr7vc2</a></td>
+    <
+    /tr> <
+    tr >
+    <
+    td > 2454 < /td> <
+    td > COT04010 < /td> <
+    td > < a href = "https://cutt.ly/oo6fVpK"
+target = "_blank" > https: //cutt.ly/oo6fVpK</a></td>
+    <
+    td > < a href = "https://cutt.ly/jfhV5cv"
+target = "_blank" > https: //cutt.ly/jfhV5cv</a></td>
+    <
+    td > < a href = "https://cutt.ly/to6dmAj"
+target = "_blank" > https: //cutt.ly/to6dmAj</a></td>
+    <
+    td > < a href = "https://cutt.ly/zo6gZAk"
+target = "_blank" > https: //cutt.ly/zo6gZAk</a></td>
+    <
+    td > < a href = "https://cutt.ly/nptmG1x"
+target = "_blank" > https: //cutt.ly/nptmG1x</a></td>
+    <
+    /tr> <
+    tr >
+    <
+    td > 4 < /td> <
+    td > OVI73510 < /td> <
+    td > < a href = "https://cutt.ly/QpeZxAh"
+target = "_blank" > https: //cutt.ly/QpeZxAh</a></td>
+    <
+    td > < a href = "https://cutt.ly/GfhBwvz"
+target = "_blank" > https: //cutt.ly/GfhBwvz</a></td>
+    <
+    td > < a href = "https://cutt.ly/0peZRdU"
+target = "_blank" > https: //cutt.ly/0peZRdU</a></td>
+    <
+    td > < a href = "https://cutt.ly/9peXSME"
+target = "_blank" > https: //cutt.ly/9peXSME</a></td>
+    <
+    td > < a href = "https://cutt.ly/9ptiLq6"
+target = "_blank" > https: //cutt.ly/9ptiLq6</a></td>
+    <
+    /tr> <
+    tr >
+    <
+    td > 1 < /td> <
+    td > OVI73510 < /td> <
+    td > < a href = "https://cutt.ly/FpeLSmN"
+target = "_blank" > https: //cutt.ly/FpeLSmN</a></td>
+    <
+    td > < a href = "https://cutt.ly/AfhBo7V"
+target = "_blank" > https: //cutt.ly/AfhBo7V</a></td>
+    <
+    td > < a href = "https://cutt.ly/4peZtv9"
+target = "_blank" > https: //cutt.ly/4peZtv9</a></td>
+    <
+    td > < a href = "https://cutt.ly/hpeXpxo"
+target = "_blank" > https: //cutt.ly/hpeXpxo</a></td>
+    <
+    td > < a href = "https://cutt.ly/JptfENs"
+target = "_blank" > https: //cutt.ly/JptfENs</a></td>
+    <
+    /tr> <
+    /table> <
+    /div>
 )
