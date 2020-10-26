5ee104c8aa1452773d09647d6f9733526fa0da7c
Jesus_0414 <jesus140400@gmail.com>
2020-10-25 18:02:02 -0700

    table center

--- src/components/map.js
+++ src/components/map.js
@@ -4,36 +4,31 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const styles = {
-    wrapper: {
-        height: 400,
-        width: '80%',
-        margin: '0 auto',
-        display: 'flex'
+    wrapper: { 
+      height: 400, 
+      width: '80%', 
+      margin: '0 auto', 
+      display: 'flex' 
     },
     map: {
-        flex: 1
-    }
-};
-
-const Moves = props => {
-    return ( <
-        div style = { styles.wrapper } >
-        <
-        Map style = { styles.map }
-        center = { props.center }
-        zoom = { props.zoom } >
-        <
-        TileLayer url = { props.url }
-        /> <
-        /Map> <
-        /div>
+      flex: 1
+    } 
+  };
+  
+  const Moves = props => {
+    return (
+      <div style={styles.wrapper}>
+        <Map style={styles.map} center={props.center} zoom={props.zoom}>
+          <TileLayer url={props.url} />
+        </Map>
+      </div>
     );
-}
-
-Moves.defaultProps = {
+  }
+  
+  Moves.defaultProps = {
     center: [30.523872138888887, -109.62057794444443],
     zoom: 10,
     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-};
-
-export default Moves;
+  };
+  
+  export default Moves;
