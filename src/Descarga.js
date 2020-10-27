f2f9ceccaeb4fa793ff844dbac740b811f5a2c1f
Hector M Gerardo <hectormgerardo@gmail.com>
2020-10-26 18:18:53 -0700

    todo fine

--- src/Descarga.js
+++ src/Descarga.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.css';
+import Original from './images/original.jpg';
+import Segmentada from './images/segmentada.png';
+import Detectada from './images/detectada.jpg';
+import Termica from './images/termica.jpg';
 
 export const Descarga = () => (
     <div>
@@ -8,12 +12,12 @@ export const Descarga = () => (
         <p>Aquí se muestra un ejemplo de 3 imágenes de la estructura 2730 en la cual se detectó una anomalía, las 3 imágenes son: Color real RGB, Imagen con detección por modelo de inteligencia artificial e imagen Térmica FLIR</p>
         <p>3.2) Estructura 2730</p>
         <p>Imagen original: Imagen RGB Color real tomada por la cámara del drone.</p>
-        <img class="img" src="/images/original.jpg"></img>
+        <img className="img" src={Original} alt="_"></img>
         <p>Imagen segmentada: Imagen analizada con inteligencia artificial para segmentar los componentes que pertenecen a la estructura y e ignorar los que no (por eso es que se ilustra el fondo en blanco y negro)</p>
-        <img class="img" src="/images/segmentada.png"></img>
+        <img className="img" src={Segmentada} alt="_"></img>
         <p>Imagen detectada: Resultado de la detección por clasificación supervisada de anomalías en la estructura.</p>
-        <img class="img" src="/images/detectada.jpg"></img>
+        <img className="img" src={Detectada} alt="_"></img>
         <p>Imagen Térmica: Imagen Térmica FLIR analizada para puntos fríos y calientes.</p>
-        <img class="img" src="/images/termica.jpg"></img>
+        <img className="img" src={Termica} alt="_"></img>
     </div>
 )
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
 import styled from 'styled-components';
+import Logo from '../images/logomedia.png'
 
 const Styles = styled.div`
   .navbar {
@@ -22,7 +23,7 @@ const Styles = styled.div`
 export const NavigationBar = () => (
   <Styles>
     <Navbar expand="lg">
-      <Navbar.Brand href="/"><img class="logo" src="/images/logomedia.png"></img></Navbar.Brand>
+      <Navbar.Brand href="/"><img class="logo" src={Logo}></img></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
